19098b1dbc6cbfd5a8b7f42bf461353c9821c5bb
eurico77 <euricomagalhaes61@gmail.com>
2023-04-24 08:34:15 -0300

    chore: create a useCase and entities for domain rules signup

---
+++ src/infra/repository/memory/UserRepositoryMemory.ts
@@ -0,0 +1,16 @@
+import UserRepository from "../../../application/repository/UserRepository";
+import User from "../../../domain/entity/User";
+
+export default class UserRepositoryMemory implements UserRepository {
+  private users: User[] = [];
+
+  async save(user: User): Promise<void> {
+    this.users.push(user);
+  }
+
+async getByEmail(email: string): Promise<User | undefined> {
+		return this.users.find((user: User) => user.email.getValue() === email);
+	}
+
+ 
+}
---
+++ test/Signup.test.ts
@@ -0,0 +1,15 @@
+import UserRepositoryMemory from "../src/infra/repository/memory/UserRepositoryMemory"
+
+
+
+describe('Signup', () => {
+  test('Deve fazer um signup', async () => {
+  const userRepository = new UserRepositoryMemory()
+  const signup = new Signup(userRepository)
+  
+  
+  })
+
+
+
+})
